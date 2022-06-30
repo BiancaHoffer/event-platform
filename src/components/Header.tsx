00fa9ff7e5235c4f00ed58ddfbc14e989ac2daea
Bianca Hoffer <99914904+BiancaHoffer@users.noreply.github.com>
2022-06-29 21:34:25 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,11 +11,12 @@ export function Header({open, setOpen}: HeaderProps){
   
   return(
     <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
-      
       <div className='w-full flex flex-row justify-between'>
         <div className="pl-10"></div>
         
-        <div><Logo/></div>
+        <div>
+          <Logo/>
+        </div>
         
         <button className='pr-10' onClick={() => setOpen(!open)}>
           { open ? <X size={32} /> : <List size={32} /> }
